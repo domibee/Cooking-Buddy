15f65e306b6d336e87cc68663294c77f5d416f5a
Nikki <dominiquelcruz@gmail.com>
2023-08-11 23:09:07 -0700

    login form added

--- forms.py
+++ forms.py
@@ -3,7 +3,11 @@ from wtforms import StringField, PasswordField, SelectField
 from wtforms.validators import InputRequired, Length, DataRequired
 
 class UserForm(FlaskForm):
-    
+    display_name = StringField('Your Display Name',validators=[InputRequired()])
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max =20)])
+    password = PasswordField('Password', validators=[InputRequired()])
+
+class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max =20)])
     password = PasswordField('Password', validators=[InputRequired()])
     
